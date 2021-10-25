a006eedeb7a77d79a94287154a5445b70ed0d252
Danylo Desiatnyk <danylode@tahometer.com>
2021-10-25 12:49:26 +0300

    Add game login in ticTacToeServer

---
+++ ticTacToeClient.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ticTacToeClient.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ticTacToeServer/Game/StatesEnum.cs
@@ -0,0 +1,9 @@
+namespace ticTacToeServer.Game
+{
+    public enum State
+    {
+        Win,
+        Draw,
+        NextMove,
+    }
+}
